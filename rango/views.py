e9b6aa86302da084766553285a8f36aa111bda43
Jeval Halton <>
2024-02-08 14:26:53 +0000

    10 pre

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 
 def index(request):
@@ -14,11 +15,18 @@ def index(request):
     context_dict["categories"] = category_list
     context_dict["pages"] = Page.objects.order_by("-views")[:5]
 
-    return render(request, "rango/index.html", context=context_dict)
+    visitor_cookie_handler(request)
+
+    response = render(request, "rango/index.html", context=context_dict)
+
+    return response
 
 
 def about(request):
     context_dict = {"name": "name"}
+
+    visitor_cookie_handler(request)
+    context_dict["visits"] = request.session["visits"]
 
     return render(request, "rango/about.html", context=context_dict)
 
@@ -169,3 +177,25 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse("rango:index"))
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get("visits", 1))
+
+    last_visit_cookie = request.COOKIES.get("last_visit", str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session["last_visit"] = str(datetime.now())
+    else:
+        request.session["last_visit"] = last_visit_cookie
+
+    request.session["visits"] = visits
